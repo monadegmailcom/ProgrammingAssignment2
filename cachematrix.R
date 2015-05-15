8292d47606cf8e3653d02405c13db6586dd77904
monade <monade@gmail.com>
2015-05-15 19:18:41 +0200

    assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+# A cached version of the matrix inversion operation for quadratic matrices.
+# Avoids to recompute the inverse if it was computed before
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Make a cached version of the provided matrix. Use this version for inverse
+# calculation "cacheSolve" to make use of caching.
+makeCacheMatrix <- function(x = matrix()) 
+{
+  # the cached version of the inverse matrix
+  invM <- NULL
+  
+  # setter function for the cached matrix
+  set <- function( y ) 
+  {
+    # bind new value
+    x <<- y
+    # reset cached inverse matrix
+    invM <<- NULL
+  }
+  
+  # getter function chached matrix
+  get <- function() x
+  
+  # setter function for inverse matrix
+  setInv <- function( v ) invM <<- v
+  
+  # getter function for inverse matrix
+  getInv <- function() invM
+  
+  # return list
+  list( set = set, get = get, setInv = setInv, getInv = getInv )
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# invert a matrix or return previously calculated result if cached
+cacheSolve <- function(x, ...) 
+{
+  # get cached result
+  invM <- x$getInv()
+  
+  # result cached?
+  if (!is.null(invM))
+  {
+    message( "getting cached data" )
+    
+    # return cached value and exit function 
+    return (invM)
+  }
+  
+  # otherwise calculate and cache result
+  
+  # calculate inverse matrix
+  invM <- solve( x$get())
+  
+  # cache inverse matrix
+  x$setInv( invM )
+  
+  # return result
+  invM
 }
